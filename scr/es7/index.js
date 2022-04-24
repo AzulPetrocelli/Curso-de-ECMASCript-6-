e9d34cd0c8674c3f04528fae72ec6d59841813ed
Azul Petrocelli <azulpetrocelli81@gmail.com>
2022-04-24 15:06:24 -0300

    ¿Qué se implementó en ES7?

---
+++ scr/es7/index.js
@@ -0,0 +1,24 @@
+//¿Qué se implementó en ES7?-----------------------------------------------------------------------------------------
+
+//Metodo includes()
+//lo que hace es retornar true o false dependiendo de si el
+//valor que le especificamos se encuentra dentro del array o no
+
+let numbers = [1,2,3,4,5,6,7,8,9,10];
+
+if (numbers.includes(7)){
+  console.log("Si se encuentra el valor 7");
+} else {
+  console.log("No se encuentra el valor 7");
+}
+
+
+//Numeros elevados
+
+let base = 4;
+let exponente = 2;
+
+//esto es igual a cuatro elevado a la dos
+let resultado = base ** exponente;
+
+console.log(resultado);// 16
