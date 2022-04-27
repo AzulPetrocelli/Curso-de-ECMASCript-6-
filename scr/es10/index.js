c03fa86bbd5268e2fbe27863823874d5fe258997
Azul Petrocelli <azulpetrocelli81@gmail.com>
2022-04-27 16:28:46 -0300

    ¿Qué se implementó en ES10?

---
+++ scr/es10/index.js
@@ -0,0 +1,47 @@
+let array 
+
+//flat() y flatMap()-------------------------------------------------------------------------------------------------
+array = [1,2,3, [1,2,3, [1,2,3]]];
+
+console.log(array.flat(1));//[1,2,3,1,2,3, [1,2,3]]
+console.log(array.flat(2));//[1,2,3,1,2,3,1,2,3]
+
+
+array = [1,2,3,4,5]; 
+
+console.log(array.flatMap(value => value, value * 2))//[1,2,2,4,3,6,4,8,5,10]
+
+
+//Object.entries-----------------------------------------------------------------------------------------------------
+array = [['nombre', 'azul'], ['age', 19]];
+
+console.log(Object.fromEntries(array));//{nombre: 'azul', age: 19}
+
+
+//.trimStart y trimEnd-----------------------------------------------------------------------------------------------
+array = '          Hello           ';
+
+console.log(array);//'          Hello           '
+console.log(array.trimEnd());//'          Hello'
+console.log(array.trimStart());//'Hello           '
+
+//Catch antes vs ahora
+
+//antes
+try{
+
+}catch (error){
+
+}
+
+//ahora
+try{
+
+} catch{
+  error
+}
+
+//Symbol() y .description---------------------------------------------------------------------------------------------------------
+let string = 'My Symbol';
+let symbol = Symbol(string);
+console.log(symbol.description);//My Symbol
