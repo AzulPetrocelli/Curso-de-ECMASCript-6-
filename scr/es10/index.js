46cddb81e6987479eb5ded896871cde7cbe94b13
Azul Petrocelli <azulpetrocelli81@gmail.com>
2022-04-29 14:37:21 -0300

    ¿Qué se implementó en ES10?

--- scr/es10/index.js
+++ scr/es10/index.js
@@ -12,7 +12,7 @@ array = [1,2,3,4,5];
 console.log(array.flatMap(value => value, value * 2))//[1,2,2,4,3,6,4,8,5,10]
 
 
-//Object.entries-----------------------------------------------------------------------------------------------------
+//Object.fromEntries-----------------------------------------------------------------------------------------------------
 array = [['nombre', 'azul'], ['age', 19]];
 
 console.log(Object.fromEntries(array));//{nombre: 'azul', age: 19}
